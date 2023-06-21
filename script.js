6d0bd032714b7fb239439f664958c8693a79a38f
Amandeep Saxena <75635658+aman5331@users.noreply.github.com>
2023-06-21 22:40:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function threeSum(arr, target) {
+// function threeSum(arr, target) {
 // write your code here
   
-}
+// }
 
-module.exports = threeSum;
+// module.exports = threeSum;
+
+function decimalToBinary(decimalNumber) {
+  let binaryNumber = "";
+
+  while (decimalNumber > 0) {
+    binaryNumber = (decimalNumber % 2) + binaryNumber;
+    decimalNumber = Math.floor(decimalNumber / 2);
+  }
+
+  return binaryNumber;
+}
