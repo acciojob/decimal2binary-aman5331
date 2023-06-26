6f15fb4a6112884f1dcc6f35f773ec7aeb8cf3c3
Amandeep Saxena <75635658+aman5331@users.noreply.github.com>
2023-06-26 11:38:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,5 @@
 
 function decimalToBinary(decimal) {
   // Use toString() with base 2 to convert decimal to binary
-  const binary = decimal.toString(2);
-
-  return binary;
+ return decimal.toString(2);
 }
